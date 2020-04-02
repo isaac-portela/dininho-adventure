7d61b133352de40db4d66a1237747d7fb4d098aa
Your Name <you@example.com>
2020-04-01 23:11:33 -0300

    Começo do desenvolvimento do game do dininho

---
+++ js/game.js
@@ -0,0 +1,9 @@
+'use strict';
+(function (){
+    const $canvas = document.querySelector("#game-dino");
+    const contexto  = $canvas.getContext("2d");
+
+
+    const  floor = new Image();
+    floor.src = '/game/chao';
+})();
