c68aebd45a801bb668d78d0f050409247b39030e
Your Name <you@example.com>
2020-04-02 00:28:19 -0300

    Adicionado o chao do game

--- js/game.js
+++ js/game.js
@@ -1,9 +1,20 @@
 'use strict';
-(function (){
-    const $canvas = document.querySelector("#game-dino");
+(function (win,doc){
+    const screen = {
+        width: win.innerWidth,
+        height: win.innerHeight
+    }
+    const $canvas = doc.querySelector("#game-dino");
     const contexto  = $canvas.getContext("2d");
-
-
     const  floor = new Image();
-    floor.src = '/game/chao';
-})();
+
+    $canvas.width = screen.width;
+    $canvas.height = screen.height;
+   
+  
+    floor.src = "/imagens/game/chao.png";
+    floor.addEventListener('load',function(){
+        contexto.drawImage(floor , 0 , screen.height -  floor.height);
+    });
+   
+})(window,document);
